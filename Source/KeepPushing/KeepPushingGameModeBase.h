0a46b523a55e78460252cb5fd39a4e6b6879cf4b
Loh10 <naturellement.toi@gmail.com>
2025-05-21 13:43:47 +0200

    Initial commit

---
+++ Source/KeepPushing/KeepPushingGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KeepPushingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEEPPUSHING_API AKeepPushingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
