dbcffc3a29dd2a6c38f51387102151b1399920b8
MattC-bit <58609954+MattC-bit@users.noreply.github.com>
2025-05-23 11:51:16 +0200

    feat timer: add C++ timer component and UI controls

---
+++ Source/KeepPushing/Private/TempPlayerController/MyPlayerController.cpp
@@ -0,0 +1,26 @@
+﻿#include "TempPlayerController/MyPlayerController.h"
+
+AMyPlayerController::AMyPlayerController()
+{
+	TimerComp = CreateDefaultSubobject<UTimerComponent>(TEXT("TimerComponent"));
+}
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (TimerComp)
+	{
+		TimerComp->StartTimer();
+	}
+
+	if (TimerWidgetClass)
+	{
+		TimerWidget = CreateWidget<UTimerUserWidget>(this, TimerWidgetClass);
+		if (TimerWidget)
+		{
+			TimerWidget->AddToViewport();
+			TimerWidget->InitTimer(TimerComp);
+		}
+	}
+}
---
+++ Source/KeepPushing/Public/TempPlayerController/MyPlayerController.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Timer/TimerComponent.h"
+#include "Timer/TimerUserWidget.h"
+#include "MyPlayerController.generated.h"
+
+UCLASS()
+class KEEPPUSHING_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AMyPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category="Timer")
+	UTimerComponent* TimerComp;
+
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<UTimerUserWidget> TimerWidgetClass;
+
+private:
+	UPROPERTY()
+	UTimerUserWidget* TimerWidget;
+};
