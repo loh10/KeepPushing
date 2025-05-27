fbd0cfc60ddc6de4664494ee1a8caf8f7667b4c3
Mathieu-Laval <92300239+Mathieu-LaHordeSchool@users.noreply.github.com>
2025-05-27 14:45:26 +0200

    Update Timer

--- Source/KeepPushing/Public/Timer/Timer.h
+++ Source/KeepPushing/Public/Timer/Timer.h
@@ -2,15 +2,17 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "TimerComponent.generated.h"
+#include "Timer.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class KEEPPUSHING_API UTimerComponent : public UActorComponent
+class UTimerUserWidget;
+
+UCLASS()
+class KEEPPUSHING_API ATimer : public AActor
 {
 	GENERATED_BODY()
 
 public:
-	UTimerComponent();
+	ATimer();
 
 	UFUNCTION(BlueprintCallable, Category="Timer")
 	void StartTimer();
@@ -24,9 +26,20 @@ public:
 	UFUNCTION(BlueprintCallable, Category="Timer")
 	FString GetFormattedTime() const;
 
+	UFUNCTION(BlueprintCallable, Category = "Timer")
+	float GetElapsed() const { return Elapsed; }
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Timer|UI")
+	TSubclassOf<UTimerUserWidget> TimerWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UTimerUserWidget> TimerWidget;
+	
 protected:
 	virtual void BeginPlay() override;
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	virtual void Tick(float DeltaTime) override;
 
 private:
 	float Elapsed = 0.f;
