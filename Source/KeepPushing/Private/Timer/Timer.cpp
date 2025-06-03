e2d0cc4d0970dd07672f80ccc3b8f29882dc28d7
kalderlunt <kalderlunt@gmail.com>
2025-06-03 15:36:08 +0200

    fix add 3 numbers : timer

--- Source/KeepPushing/Private/Timer/Timer.cpp
+++ Source/KeepPushing/Private/Timer/Timer.cpp
@@ -31,8 +31,12 @@ void ATimer::Tick(float DeltaTime)
 
 FString ATimer::GetFormattedTime() const
 {
-	int32 TotalSec = FMath::FloorToInt(Elapsed);
+	int32 totalCentiseconds = FMath::FloorToInt(Elapsed * 1000);
+	int32 TotalSec = totalCentiseconds / 1000;
+	
 	int32 Min = TotalSec / 60;
 	int32 Sec = TotalSec % 60;
-	return FString::Printf(TEXT("%02d:%02d"), Min, Sec);
+	int32 Centi = totalCentiseconds % 1000;
+
+	return FString::Printf(TEXT("%02d:%02d:%03d"), Min, Sec, Centi);
 }
