276c58a42c4ce8a848080e44f7877777fe06d2a9
kalderlunt <kalderlunt@gmail.com>
2025-06-04 14:01:00 +0200

    feat void zone

---
+++ Source/KeepPushing/Public/VoidZone/VoidZone.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VoidZone.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnVoidZoneOverlap, AActor*, OverlappedActor);
+
+UCLASS()
+class KEEPPUSHING_API AVoidZone : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AVoidZone();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* _triggerZone;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* overlappedComp,
+		AActor* otherActor,
+		UPrimitiveComponent* otherComp,
+		int32 otherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& sweepResult);
+
+public:
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnVoidZoneOverlap OnVoidZoneTouched;
+
+	virtual void Tick(float DeltaTime) override;
+};
