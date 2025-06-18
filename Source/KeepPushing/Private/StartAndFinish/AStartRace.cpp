7751a89a55d62805462b73d2bb62854cd975dc72
Loh10 <naturellement.toi@gmail.com>
2025-06-18 11:49:49 +0200

    Add Loading screen

--- Source/KeepPushing/Private/StartAndFinish/AStartRace.cpp
+++ Source/KeepPushing/Private/StartAndFinish/AStartRace.cpp
@@ -20,7 +20,7 @@ void AAStartRace::BeginPlay()
 
 void AAStartRace::BeginCountdown()
 {
-	_currentCount = _countdownStart;
+	_currentCount = _countdownStart + _offsetCountdown;
 	UpdateCountdown();
 	GetWorldTimerManager().SetTimer(_countdownTimer, this, &AAStartRace::UpdateCountdown, 1.f, true);
 }
